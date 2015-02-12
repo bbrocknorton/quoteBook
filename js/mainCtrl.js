175c4336a3124b8e7cc6abd279b57567a9719c74
Brock Norton <bbrocknorton@gmail.com>
2015-02-11 22:16:28 -0700

    Quote Book updated, need to add Cookies

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,5 +1,27 @@
 var app = angular.module('quoteBook');
 
-app.controller('mainCtrl', function($scope, mainService) {
-	$scope.test = mainService.getUsers();
+
+
+app.controller('mainCtrl', function($scope, dataService) {
+
+
+	$scope.showAddQuote = 1;
+
+	var getData = function() {
+		$scope.quotes = dataService.getData()
+
+	}
+	getData();
+
+	$scope.addData = function(text, author) {
+		dataService.addData({
+			"text": text,
+			"author": author
+		});
+	}
+
+	$scope.removeData = function(text) {
+		dataService.removeData(text);
+	}
+
 });
